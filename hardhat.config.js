ddcc65e165fac19adf1115a75a688210745b542f
Saing Sab (Ayoung) <saing.sab@gmail.com>
2022-08-08 16:30:19 +0700

    deployed and verify

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,25 @@
-require("@nomicfoundation/hardhat-toolbox");
+require("@nomiclabs/hardhat-waffle");
+require("@nomiclabs/hardhat-etherscan");
+require("dotenv").config();
+
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.9",
+  solidity: "0.8.4",
+  networks: {
+    hardhat: {
+      chainId: 1337,
+    },
+    mumbai: {
+      url: process.env.STAGING_INFURA_URL,
+      accounts: [`0x${process.env.STAGING_PRIVATE_KEY}`],
+      gas: 2100000,
+      gasPrice: 80000000000,
+    }
+  },
+  etherscan: {
+    apiKey: {
+      polygonMumbai: process.env.apiKey    }
+  } 
 };
+  
